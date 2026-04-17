ce954a57f605fd6ee4bd803f7e4372904ad3ab4e
aflanazeezul-rgb <aflanazeezul@gmail.com>
2026-04-17 10:38:29 +0530

    UC1: Feet Measurement Equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,57 @@
 public class QuantityMeasurementApp {
+
+
+        // Inner class to represent Feet measurement
+        static class Feet {
+            private final double value;
+
+            // Constructor
+            public Feet(double value) {
+                this.value = value;
+            }
+
+            public double getValue() {
+                return value;
+            }
+
+            // Override equals() method
+            @Override
+            public boolean equals(Object obj) {
+
+                // Same reference check (Reflexive)
+                if (this == obj) {
+                    return true;
+                }
+
+                // Null and type check
+                if (obj == null || getClass() != obj.getClass()) {
+                    return false;
+                }
+
+                // Type casting
+                Feet other = (Feet) obj;
+
+                // Compare double values safely
+                return Double.compare(this.value, other.value) == 0;
+            }
+
+            // Optional: override hashCode (good practice when equals is overridden)
+            @Override
+            public int hashCode() {
+                return Double.hashCode(value);
+            }
+        }
+
+        // Main method to test
+        public static void main(String[] args) {
+
+            Feet f1 = new Feet(1.0);
+            Feet f2 = new Feet(1.0);
+
+            boolean result = f1.equals(f2);
+
+            System.out.println("Input: 1.0 ft and 1.0 ft");
+            System.out.println("Output: Equal (" + result + ")");
+        }
 }
+
